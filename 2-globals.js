fdb88b2bd30f25700766995b46084585a670cf1b
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-08-25 21:53:56 +1000

    Add 2-globals.js

---
+++ 2-globals.js
@@ -0,0 +1,9 @@
+// GLOBAL - NO WINDOW !!!
+
+// _dirname - path to the current directory
+// _filename - file name
+// require - function to use module (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log();
