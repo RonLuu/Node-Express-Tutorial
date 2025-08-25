17845c80293bcc8ad0de599a16a0c84c021c1420
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-08-26 08:41:47 +1000

    Add 7-mind-grenade.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 const name = require('./4-name')
 const sayHi = require('./5-utils')
 const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+// console.log(name)
+// console.log(data);
 
-console.log(name)
-console.log(data);
-
-sayHi("Susan")
-sayHi(name.john)
-sayHi(name.peter)
+// sayHi("Susan")
+// sayHi(name.john)
+// sayHi(name.peter)
