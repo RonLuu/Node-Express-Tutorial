639eb554d34d8e67e406b6fba02ae63643a6da23
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-08-26 08:49:24 +1000

    Add 3-modules.js

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+// CommonJS - every file is a module
+// Modules - Encapsulated Code
+const name = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+// console.log(name)
+// console.log(data);
+
+// sayHi("Susan")
+// sayHi(name.john)
+// sayHi(name.peter)
