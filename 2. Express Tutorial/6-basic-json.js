52d90c5a4644acfd9cede007689b33cd4f1cedf9
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-02 20:39:39 +1000

    Add 6-basic-json.js

---
+++ 2. Express Tutorial/6-basic-json.js
@@ -0,0 +1,14 @@
+const expire = require("express")
+const app = expire()
+const products = require("./data")
+app.get("/", (req, res) =>
+    {
+        res.json(products)
+    }
+)
+
+app.listen(5000, () =>
+    {
+        console.log("Listening on port 5000")
+    }
+)
